6f2f3f5bf11e9001be2558a56f72d6e912055f20
Aleksandr <33601912+alexandrtiunov@users.noreply.github.com>
2017-11-22 19:39:06 +0200

    Сount elements of array

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$array = [92, 'name' => 'Alex', 'surname' => 'Sergey', 17, 46, 2, 15, 43, 65, 86, 27];
+$array = [92, 99, 28, 17, 46, 2, 15, 43, 65, 86, 27, 24, 56];
 
 /**
  * This function count elements of array
@@ -8,12 +8,15 @@ $array = [92, 'name' => 'Alex', 'surname' => 'Sergey', 17, 46, 2, 15, 43, 65, 86
  * @param $array
  * @return int
  */
-function arrayCount($array)
+function arrayCount($array) // определяем функцию
 {
-    $result = count($array);
-    return $result;
+    $arrayCount = 1; // Счетчик, который будет считать элементы массива
+    foreach ($array as $value){  // проходим по всем элементам массива
+        $result = $arrayCount ++; // в переменную $result вносим колличество элементов массива
+    }
+    return $result; // возвращаем $result
 }
 
-echo 'в массиве ' . arrayCount($array) . ' элементов!';
+echo 'в массиве ' . arrayCount($array) . ' элементов!'; // выводим значение на экран
 
 ?>
