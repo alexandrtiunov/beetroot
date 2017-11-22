e09b0ae444fb7f0c004376ee0890a89d812669b6
Aleksandr <33601912+alexandrtiunov@users.noreply.github.com>
2017-11-22 19:46:08 +0200

    function MultipleOfFive

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-$array = [92, 15, 46, 25, 15, 43, 65, 86, 27];
+$array = [92, 15, 46, 25, 95, 45, 65, 86, 27, 155];
 
 /**
  * This function MultipleOfFive
  * @param $array
  */
-function MultipleOfFive($array)
+function MultipleOfFive($array) // определяем функцию
 {
-    for ($i = 0; $i < count($array); $i++)
-        if (($array[$i]%5) == 0){
-        echo 'Значения кратные  пяти в Вашем массиве: это ' . $array[$i] . '<br>';
+    for ($i = 0; $i < $array[$i]; $i++) // проходим по массиву с помощью цикла for
+        if (($array[$i]%5) == 0){ // определяем какой из элементов массива кратен 5, т.е делется на 5 без остатка
+        echo 'Значения кратные  пяти в Вашем массиве: это ' . $array[$i] . '<br>'; // выводим значения массива на экран
         }else {}
         return ' ';
 
