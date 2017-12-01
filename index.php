3a4b491ed13f863f453d436d0dbc9b133e7d9b3c
Aleksandr <33601912+alexandrtiunov@users.noreply.github.com>
2017-12-01 20:36:16 +0200

    сlassWork01.12

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <td width="200" height="200" align="right">
-                <form action="feedback.php" method="post" enctype="multipart/form-data">
-                    <p>
-                        Имя: <input type="text" name="name" required>
-                    </p>
-                    <p>
-                        Email: <input type="email" name="email" required>
-                    </p>
-                    <p>
-                        Текст: <input type="text" name="text" maxlength="200" required>
-                    </p>
-                    <p>
-                        <input type="file" name="file">
-                    </p>
-                    <p>
-                        <button type="submit">Отправить заявку</button>
-                    </p>
-                </form>
-            </td>
-        </tr>
-    </table>
-</body>
-</html>
+<?php
+
+//$numbers = [1, 2, 3, 4, 5];
+//$sum = 0;
+//
+//foreach ($numbers as $value){
+//
+//    $sum = $sum + pow($value,2);
+//    echo $sum . '</br>';
+//}
+//
+//var_dump($sum);
+
+$array = []; //
+$numberFormats = [];
+
+for ($i = 0; $i < 100; $i++){
+    $array[] = rand(0, 100);
+}
+
+
+echo '<pre>';
+print_r($array);
+echo '<pre>';
+
+
+foreach ($array as $value){
+    $numberFormats[] = number_format(log10($value) ,3);
+}
+
+echo '<pre>';
+print_r($numberFormats);
+echo '<pre>';
+
+?>
